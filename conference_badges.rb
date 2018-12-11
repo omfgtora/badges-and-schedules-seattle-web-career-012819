fd5ec6c53ed90479f1cb381fc1c1f243181f4805
Ethan Roberts <ejrobe@gmail.com>
2018-12-11 06:30:27 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  x = Array.new()
+  
+  for each in attendees
+    x.push("Hello, my name is #{each}.")
+  end
+  
+  return x
+end
+
+def assign_rooms(attendees)
+  room = 1
+  x = Array.new()
+  for each in attendees
+    x.push("Hello, #{each}! You'll be assigned to room #{room}!")
+    room += 1
+  end
+  return x
+end
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  rooms = assign_rooms(attendees)
+  for each in badges
+    puts each
+  end
+  for each in rooms
+    puts each
+  end
+end
